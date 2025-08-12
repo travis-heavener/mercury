6275cd9f3d1f9ed984403a49c2f4922d8d3260fa
Travis Heavener <travis.heavener@gmail.com>
2025-08-12 11:57:09 -0400

    Large artifacts now compilable for independent platforms

--- build_tools/build_static_brotli.sh
+++ build_tools/build_static_brotli.sh
@@ -22,49 +22,54 @@ fi
 # Clone Brotli repo
 git clone https://github.com/google/brotli ./brotli-repo
 cd brotli-repo
+mkdir -p $LIB_PATH/brotli
 mkdir build && cd build
 
 # ==== Linux Build ====
 
-# Prepare CMAKE for compilation
-cmake .. \
+if [ -z "$1" ] || [ "$1" == "linux" ]; then
+    # Prepare CMAKE for compilation
+    cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DBROTLI_BUNDLED_MODE=ON \
     -DBUILD_SHARED_LIBS=OFF
 
-# Build
-make
+    # Build
+    make
 
-# Extract resources
-mkdir $LIB_PATH/brotli $LIB_PATH/brotli/linux $LIB_PATH/brotli/linux/lib
+    # Extract resources
+    mkdir $LIB_PATH/brotli/linux $LIB_PATH/brotli/linux/lib
 
-mv *.a $LIB_PATH/brotli/linux/lib
-cp -r $LIB_PATH/brotli-repo/c/include $LIB_PATH/brotli/linux/include
+    mv *.a $LIB_PATH/brotli/linux/lib
+    cp -r $LIB_PATH/brotli-repo/c/include $LIB_PATH/brotli/linux/include
+fi
 
+cd ..
 # ==== Windows Build ====
 
 # Wipe build dir
-cd ..
-rm -rf build
-mkdir build && cd build
+if [ -z "$1" ] || [ "$1" == "windows" ]; then
+    rm -rf build
+    mkdir build && cd build
 
-# Set Windows cross-compiling with MinGW
-cmake .. \
-    -DCMAKE_SYSTEM_NAME=Windows \
-    -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
-    -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBROTLI_BUNDLED_MODE=ON \
-    -DBUILD_SHARED_LIBS=OFF
+    # Set Windows cross-compiling with MinGW
+    cmake .. \
+        -DCMAKE_SYSTEM_NAME=Windows \
+        -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
+        -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DBROTLI_BUNDLED_MODE=ON \
+        -DBUILD_SHARED_LIBS=OFF
 
-# Build
-make
+    # Build
+    make
 
-# Extract headers & linker files
-mkdir $LIB_PATH/brotli/windows $LIB_PATH/brotli/windows/lib
+    # Extract headers & linker files
+    mkdir $LIB_PATH/brotli/windows $LIB_PATH/brotli/windows/lib
 
-mv *.a $LIB_PATH/brotli/windows/lib
-mv $LIB_PATH/brotli-repo/c/include $LIB_PATH/brotli/windows/include
+    mv *.a $LIB_PATH/brotli/windows/lib
+    mv $LIB_PATH/brotli-repo/c/include $LIB_PATH/brotli/windows/include
+fi
 
 # ==== Clean Up ====
 cd $LIB_PATH
