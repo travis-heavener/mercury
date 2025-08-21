7e81ee14d657d3898a77d0c6a71059e14e245bc9
Travis Heavener <travis.heavener@gmail.com>
2025-08-21 15:47:56 -0400

    Improve release generation logic, fix broken modals

--- docs/index.js
+++ docs/index.js
@@ -7,7 +7,7 @@ $(() => {
 const handleReleaseJSON = (releases) => {
     // Iteratively append to DOM
     const urlPrefix = "https://github.com/travis-heavener/mercury/releases/tag/";
-    const urlDownloadPrefix = "https://github.com/travis-heavener/mercury/releases/download/";
+    const downloadPrefix = "https://github.com/travis-heavener/mercury/releases/download/";
 
     const jWindowsTable = $("#downloads-windows-table > tbody");
     const jLinuxTable = $("#downloads-linux-table > tbody");
@@ -15,53 +15,42 @@ const handleReleaseJSON = (releases) => {
     // Filter anything but the 10 most recent releases
     releases = releases.slice(0, 10);
 
-    for (const release of releases) {
-        // Resolve asset links
-        const winSuffix = "Windows_" + release.name.replaceAll(".", "_") + ".zip";
-        const linuxSuffix = "Linux_" + release.name.replaceAll(".", "_") + ".tar.gz";
-        const downloadWinUrl = urlDownloadPrefix + release.name + "/" + winSuffix;
-        const downloadLinuxUrl = urlDownloadPrefix + release.name + "/" + linuxSuffix;
+    // Row maker helper function
+    const genRow = (name, timeString, downloadUrl, suffix, shortHash, fullHash) => `
+            <tr>
+                <td><a href="${urlPrefix + name}">${name}</a></td>
+                <td>${timeString}</td>
+                <td><a href="${downloadUrl}">${suffix}</a></td>
+                <td class="hash-digest" data-full-hash=${fullHash} title="Copy to clipboard">
+                    ${shortHash}
+                    ${fullHash ? `<img src="clipboard.svg">` : ""}
+                </td>
+            </tr>
+        `;
 
-        let winHash = linuxHash = "&ndash;";
-        if (release.winHash)
-            winHash = release.winHash.substring(0, 6) + "..." + release.winHash.substring(62);
-        if (release.linuxHash)
-            linuxHash = release.linuxHash.substring(0, 6) + "..." + release.linuxHash.substring(62);
+    // Generate release rows
+    for (const { name, date, winHash, linuxHash } of releases) {
+        // Resolve asset links
+        const winSuffix = "Windows_" + name.replaceAll(".", "_") + ".zip";
+        const linuxSuffix = "Linux_" + name.replaceAll(".", "_") + ".tar.gz";
+        const winUrl = downloadPrefix + name + "/" + winSuffix;
+        const linuxUrl = downloadPrefix + name + "/" + linuxSuffix;
+
+        const winShortHash = !winHash ? "&ndash;" : (winHash.substring(0, 6) + "..." + winHash.substring(62));
+        const linuxShortHash = !linuxHash ? "&ndash;" : (linuxHash.substring(0, 6) + "..." + linuxHash.substring(62));
 
         // Resolve timestamp
-        const timeString = new Date(release.date)
-            .toLocaleDateString("en-US");
+        const timeString = new Date(date).toLocaleDateString("en-US");
 
         // Append to DOM
-        jWindowsTable.append(`
-            <tr>
-                <td><a href="${urlPrefix + release.name}">${release.name}</a></td>
-                <td>${timeString}</td>
-                <td><a href="${downloadWinUrl}">${winSuffix}</a></td>
-                <td class="hash-digest" data-full-hash=${release.winHash} title="${release.winHash}">
-                    ${winHash}
-                    ${release.winHash ? `<img src="clipboard.svg">` : ""}
-                </td>
-            </tr>
-        `);
-
-        jLinuxTable.append(`
-            <tr>
-                <td><a href="${urlPrefix + release.name}">${release.name}</a></td>
-                <td>${timeString}</td>
-                <td><a href="${downloadLinuxUrl}">${linuxSuffix}</a></td>
-                <td class="hash-digest" data-full-hash=${release.linuxHash} title="${release.linuxHash}">
-                    ${linuxHash}
-                    ${release.linuxHash ? `<img src="clipboard.svg">` : ""}
-                </td>
-            </tr>
-        `);
+        jWindowsTable.append(genRow(name, timeString, winUrl, winSuffix, winShortHash, winHash));
+        jLinuxTable.append(genRow(name, timeString, linuxUrl, linuxSuffix, linuxShortHash, linuxHash));
     }
 
     // Bind click events to all hash digests
     $(".hash-digest").on("click", async function() {
         await navigator.clipboard.writeText($(this).attr("data-full-hash"));
-        showPassiveModal("Copied to clipboard!");
+        showToast("Copied to clipboard!");
     });
 
     // Update the latest release content
@@ -69,28 +58,36 @@ const handleReleaseJSON = (releases) => {
         const { name } = releases[0];
         $("#latest-release-div > h3").text(name);
 
-        $("#latest-release-div > a.windows")[0].href = urlDownloadPrefix + name +
+        $("#latest-release-div > a.windows")[0].href = downloadPrefix + name +
             "/Windows_" + name.replaceAll(".", "_") + ".zip";
 
-        $("#latest-release-div > a.linux")[0].href = urlDownloadPrefix + name +
+        $("#latest-release-div > a.linux")[0].href = downloadPrefix + name +
             "/Linux_" + name.replaceAll(".", "_") + ".tar.gz";
     }
 };
 
-// Shows a passive modal on screen
-const showPassiveModal = (msg) => {
+// Shows a toast on screen
+const showToast = (msg) => {
     // Create modal
     const modal = document.createElement("DIV");
-    modal.className = "passive-modal";
+    modal.className = "toast";
 
     // Create text
     const p = document.createElement("P");
     p.innerText = msg
     modal.appendChild(p);
 
-    // Append to DOM
-    $(document.body).append(modal);
+    // Add progress bar
+    const progress = document.createElement("DIV");
+    progress.className = "progress-bar";
+    modal.appendChild(progress);
 
-    // Destroy after 3 seconds
-    setTimeout(() => $(modal).remove(), 3000);
+    document.body.appendChild(modal);
+
+    // Start fade out before removal
+    setTimeout(() => {
+        modal.classList.add("fade-out");
+        setTimeout(() => modal.remove(), 400); // matches fade duration
+    }, 3000);
+
 };
