148dcd99a267cf2db454054dafa37cbaf767f4ea
Travis Heavener <travis.heavener@gmail.com>
2025-09-04 14:39:30 -0400

    Now restores working directory

--- conf/ssl/makecert.ps1
+++ conf/ssl/makecert.ps1
@@ -1,3 +1,5 @@
+Push-Location
+
 #
 # Note: Update this path below to point to where openssl.exe is installed on your system
 #   Windows copies of Git should come installed with OpenSSL (see C:/Program Files/Git/usr/bin/openssl.exe)
@@ -9,3 +11,5 @@ Set-Location -Path $PSScriptRoot
 
 # Make cert
 & "$OPENSSL_PATH" req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes
+
+Pop-Location
