55fc6ee4a89818d1bb6428c8ad06cb629d6b019c
Travis Heavener <travis.heavener@gmail.com>
2025-10-01 18:25:24 -0400

    Revert mingw-w64 cache step
    Revert mingw-w64 cache step

--- build_tools/install_workflow_deps.sh
+++ build_tools/install_workflow_deps.sh
@@ -3,10 +3,7 @@
 # Installs *all* deps needed to rebuild Mercury
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install \
-    build-essential \
-    mingw-w64 nasm \
-    zlib1g-dev \
-    php-cgi \
+    mingw-w64 \
     -y --no-install-recommends
 
 echo "✅ Successfully installed workflow dependencies."
