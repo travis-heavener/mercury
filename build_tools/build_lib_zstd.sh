faca809f0f2fa3086cbc82d31d0071d47f2cc989
Travis Heavener <travis.heavener@gmail.com>
2025-10-22 04:15:41 -0400

    Parallelized build script (#328)

--- build_tools/build_lib_zstd.sh
+++ build_tools/build_lib_zstd.sh
@@ -29,6 +29,8 @@ version=$( cat ../build_tools/dependencies.txt | grep -Po "(?<=^ZSTD=)(.*)$" )
 
 # Clean existing
 $TOOLS_PATH/safe_rm "zstd-$version"
+$TOOLS_PATH/safe_rm "zstd-$version-linux"
+$TOOLS_PATH/safe_rm "zstd-$version-windows"
 $TOOLS_PATH/safe_rm "zstd-$version.tar.gz"
 
 # Get Zstandard source
@@ -39,37 +41,35 @@ mkdir zstd
 mkdir zstd/linux zstd/linux/include zstd/linux/lib
 mkdir zstd/windows zstd/windows/include zstd/windows/lib
 
-# Unpack tarball
+# Extract archive
 tar -xzf "zstd-$version.tar.gz"
 echo "Extracted archive."
-cd "zstd-$version/lib"
 
-# ==== Linux Build ====
+# ==== Build in Parallel ====
+cp -r "zstd-$version" "zstd-$version-windows"
+mv "zstd-$version" "zstd-$version-linux"
 
-# Make libzstd.a
-make libzstd.a
+(
+    cd "zstd-$version-linux/lib"
+    make -j$(nproc) libzstd.a 1> /dev/null
+    cp *.h "$LIB_PATH/zstd/linux/include"
+    mv libzstd.a "$LIB_PATH/zstd/linux/lib"
+) &
 
-# Copy include headers & static lib
-cp *.h "$LIB_PATH/zstd/linux/include"
-mv libzstd.a "$LIB_PATH/zstd/linux/lib"
+(
+    cd "zstd-$version-windows/lib"
+    make -j$(nproc) libzstd.a CC=x86_64-w64-mingw32-gcc 1> /dev/null
+    cp *.h "$LIB_PATH/zstd/windows/include"
+    mv libzstd.a "$LIB_PATH/zstd/windows/lib"
+) &
 
-echo "Built Linux binaries."
-
-# ==== Windows Build ====
-
-# Make libzstd.a
-make libzstd.a CC=x86_64-w64-mingw32-gcc
-
-# Copy include headers & static lib
-cp *.h "$LIB_PATH/zstd/windows/include"
-mv libzstd.a "$LIB_PATH/zstd/windows/lib"
-
-echo "Built Windows binaries."
+wait
 
 # ==== Clean Up ====
-
 cd "$LIB_PATH"
 rm -rf "zstd-$version"
+rm -rf "zstd-$version-linux"
+rm -rf "zstd-$version-windows"
 rm -f "zstd-$version.tar.gz"
 
 # Update artifacts.lock
