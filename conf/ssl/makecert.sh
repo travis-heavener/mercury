c7dc3cdda15bf8ecaa2d7fd4da8ff22e0d02cc8e
Travis Heavener <travis.heavener@gmail.com>
2025-11-15 02:22:00 -0500

    Fix syntax

--- conf/ssl/makecert.sh
+++ conf/ssl/makecert.sh
@@ -4,28 +4,13 @@
 cd "$(dirname "$0")"
 
 # Install openssl
-case "$(./tools/getpm)" in
-    apt)
-        sudo apt-get update 1>/dev/null
-        sudo apt install openssl -y 1>/dev/null
-        ;;
-    pacman)
-        sudo pacman -Sy --noconfirm 1>/dev/null
-        sudo pacman -S --noconfirm openssl 1>/dev/null
-        ;;
-    # Not implemented yet
-    # dnf)
-    #     sudo dnf makecache -y 1>/dev/null
-    #     sudo dnf install -y openssl 1>/dev/null
-    #     ;;
-    # yum)
-    #     sudo yum makecache -y 1>/dev/null
-    #     sudo yum install -y openssl 1>/dev/null
-    #     ;;
-    *)
-        echo "Unknown package manager: $PKG_MGR" >&2
-        exit 1
-        ;;
+if command -v apt >/dev/null 2>&1; then
+    sudo apt-get update 1>/dev/null
+    sudo apt-get install openssl -y 1>/dev/null
+elif command -v pacman >/dev/null 2>&1; then
+    sudo pacman -Sy --noconfirm 1>/dev/null
+    sudo pacman -S --noconfirm openssl 1>/dev/null
+fi
 
 if [ -f "key.pem" ]; then
     rm -f key.pem
