a9a1e549f206bab41849c9f57e816ca68b04cf3a
Travis Heavener <travis.heavener@gmail.com>
2025-11-15 02:12:20 -0500

    Implement dynamic package manager for makecert.sh

--- conf/ssl/makecert.sh
+++ conf/ssl/makecert.sh
@@ -4,8 +4,28 @@
 cd "$(dirname "$0")"
 
 # Install openssl
-sudo apt update
-sudo apt install openssl -y
+case "$(./tools/getpm)" in
+    apt)
+        sudo apt-get update 1>/dev/null
+        sudo apt install openssl -y 1>/dev/null
+        ;;
+    pacman)
+        sudo pacman -Sy --noconfirm 1>/dev/null
+        sudo pacman -S --noconfirm openssl 1>/dev/null
+        ;;
+    # Not implemented yet
+    # dnf)
+    #     sudo dnf makecache -y 1>/dev/null
+    #     sudo dnf install -y openssl 1>/dev/null
+    #     ;;
+    # yum)
+    #     sudo yum makecache -y 1>/dev/null
+    #     sudo yum install -y openssl 1>/dev/null
+    #     ;;
+    *)
+        echo "Unknown package manager: $PKG_MGR" >&2
+        exit 1
+        ;;
 
 if [ -f "key.pem" ]; then
     rm -f key.pem
